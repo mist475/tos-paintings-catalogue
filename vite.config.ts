9abaca5b671df045919e3cbdf4bccf09e9a53519
mist475 <70655895+mist475@users.noreply.github.com>
2026-03-03 17:42:26 +0100

    fix path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:"/tos-paintings-catalogue/",
   plugins: [
     svelte(),
     tailwindcss()
